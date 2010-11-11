5cd7e313e1ba60c1686bee0d82b7af43942c70c6
cbmeeks <cbmeeks@gmail.com>
2010-11-11 12:38:15 -0500

    Menu hover works now.  Now you can specify the UL by a data-Attribute.  :-)

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
 
 	var $dropdown = $(".dropdown");
+	var menu;
 	
-	
+	function showMenu() {
+		$(menu).show();
+	}
 
+	function hideMenu() {
+		$(menu).hide();
+	}
+	
+	$dropdown.hover(function() {
+		var self = this;
+		var left = $(self).position().left;
+		var right = $(self).position().right;
+
+		hideMenu(); // clear any previous menu hover states
+
+		menu = $("#" + self.getAttribute("data-menu"));
+		$(menu).css({"left":left, "right":right});
+		$(menu).live("mouseover", showMenu);
+		$(menu).live("mouseout", hideMenu);
+		showMenu();
+	});
+	
+	$dropdown.click(function() {
+		showMenu();
+		return false;
+	});
+	
+	$(menu).mouseover(function() {showMenu();});
+	$(menu).mouseout(function() {hideMenu();});
+	
 });
