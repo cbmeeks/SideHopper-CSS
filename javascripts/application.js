16ce3304ba32a13f22ab81d385f3c1d4a7c92c9e
cbmeeks <cbmeeks@gmail.com>
2010-11-11 13:41:36 -0500

    More tweaks.  Added content.

--- javascripts/application.js
+++ javascripts/application.js
@@ -19,17 +19,12 @@ $(document).ready(function() {
 		hideMenu(); // clear any previous menu hover states
 
 		menu = $("#" + self.getAttribute("data-menu"));
-		$(menu).css({"left":left, "right":right});
+		$(menu).css({ "left":left, "right":right });
 		$(menu).live("mouseover", showMenu);
 		$(menu).live("mouseout", hideMenu);
 		showMenu();
 	});
-	
-	$dropdown.click(function() {
-		showMenu();
-		return false;
-	});
-	
+		
 	$(menu).mouseover(function() {showMenu();});
 	$(menu).mouseout(function() {hideMenu();});
 	
