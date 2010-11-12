62f12dc0b441ea480b0024f9fe40e76e6343dbe5
cbmeeks <cbmeeks@gmail.com>
2010-11-11 19:58:31 -0500

    Added ability to correctly position menu items....even for the right toolbar.  Should work no matter how the parent toolbar is positioned.

--- javascripts/application.js
+++ javascripts/application.js
@@ -13,13 +13,14 @@ $(document).ready(function() {
 	
 	$dropdown.hover(function() {
 		var self = this;
-		var left = $(self).position().left;
-		var right = $(self).position().right;
+		var toolbar = $(self).parent().parent();	// climb up tree to toolbar parent
+		var left = $(toolbar).position().left + $(self).position().left;
+
 
 		hideMenu(); // clear any previous menu hover states
 
 		menu = $("#" + self.getAttribute("data-menu"));
-		$(menu).css({ "left":left, "right":right });
+		$(menu).css({ "left":left });
 		$(menu).live("mouseover", showMenu);
 		$(menu).live("mouseout", hideMenu);
 		showMenu();
